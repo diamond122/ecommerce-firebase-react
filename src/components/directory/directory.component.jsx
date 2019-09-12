3c08f95018d44f14659ced374a6a2279cc3f7f74
diamond-shine <focusingdev@outlook.com>
2019-09-12 02:30:42 -0400

    Added basic routes and re-named main variables

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -8,36 +8,36 @@ class Directory extends React.Component {
 
         this.state = {
             sections: [{
-                title: 'hats',
+                title: 'paintings',
     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     id: 1,
-    linkUrl: 'shop/hats'
+    linkUrl: 'shop/paintings'
   },
   {
-    title: 'jackets',
+    title: 'prints',
     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     id: 2,
-    linkUrl: 'shop/jackets'
+    linkUrl: 'shop/prints'
   },
   {
-    title: 'sneakers',
+    title: 't-shirts',
     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     id: 3,
-    linkUrl: 'shop/sneakers'
+    linkUrl: 'shop/t-shirts'
   },
   {
-    title: 'womens',
+    title: 'book',
     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: 'shop/book'
   },
   {
-    title: 'mens',
+    title: 'commissions',
     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: 'shop/commissions'
   
             }]
         }

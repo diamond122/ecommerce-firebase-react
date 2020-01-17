d5f3ae3eb15c9776095d14c10baf5c51d92b3ac9
diamond-shine <focusingdev@outlook.com>
2020-01-16 16:19:08 -0800

    Added store and user reducer files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux-dom';
+
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux-dom'
+
+
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>

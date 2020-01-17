fd932ffe195c057da79f15ec2a61651eaeb92a18
diamond-shine <focusingdev@outlook.com>
2020-01-16 18:23:17 -0800

    Added cart component icon - styled

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
@@ -16,12 +17,13 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/shop'>CONTACT</Link>
             {
-                currentUser ? 
+                currentUser ? (
                 <div className='option'
                      onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
+               ) : (
                 <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+            )}
+            <CartIcon />
         </div>
     </div>
 );

3b43c88bea1a13cf7d73efe4d43ca7ff21aa2363
diamond-shine <focusingdev@outlook.com>
2020-01-18 19:36:10 -0800

    Added graphQL schema

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
-    hidden: true    
+    hidden: true,
+    cartItems: []    
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,7 +11,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state, 
                 hidden: !state.hidden
-            }
+            };
 
         case CartActionTypes.ADD_ITEM:
             return {
